c60f12c408848e6c7de47a8147006ce67e305328
CHGP <exocorpllc@gmail.com>
2019-12-10 09:40:06 +0100

    Added speakerFiles

---
+++ speaker-Files/doorBell.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import os
+
+def on_connect(client, userdata, flags, rc):
+
+    print("Connected with result code "+str(rc))
+
+    client.subscribe("bell")
+
+def on_message(client, userdata, msg):
+
+    print(msg.topic+" "+str(msg.payload))
+    os.system("curl -X POST -H \'Content-type: application/json\' --data \'{\"text\":\"Someone is at the door... <http://192.168.10.71:8080/stream.html|Click here for video feed>\"}\' https://hooks.slack.com/services/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")
+    os.system("play BingBong.mp3 vol 9")
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+
+client.on_message = on_message
+
+
+client.connect("192.168.10.71")
+
+client.loop_forever()
